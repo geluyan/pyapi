d03dbc63f73d9ded5031d69522925c4ab8f0eadf
geluyan <a.geluykens@gmail.com>
2019-03-26 14:53:34 +0100

    completed test functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import urllib3
 import unittest
 import pythapi
 
-# The following test are written with the help of https://jsonplaceholder.typicode.com
+# The following test are written with the help of https://my-json-server.typicode.com
 # Tests are based on the Python default unittest module
 
 class testPythapi(unittest.TestCase):
@@ -12,9 +12,9 @@ class testPythapi(unittest.TestCase):
         """
         # Disable certificate warnings and connect to Rubrik Cluster
         urllib3.disable_warnings()
-        jsonplaceholder = pythapi.Connect('jsonplaceholder.typicode.com', '')
+        jsonplaceholder = pythapi.Connect('my-json-server.typicode.com', '/geluyan/pythapi')
 
-        name = "Leanne Graham"
+        name = "geluykens"
         user = jsonplaceholder.get("/users/1", authentication=False)
 
         self.assertEqual(name, user['name'])
@@ -25,12 +25,45 @@ class testPythapi(unittest.TestCase):
         """
         # Disable certificate warnings and connect to Rubrik Cluster
         urllib3.disable_warnings()
-        jsonplaceholder = pythapi.Connect('jsonplaceholder.typicode.com', '')
+        jsonplaceholder = pythapi.Connect('my-json-server.typicode.com', '/geluyan/pythapi')
 
-        data = "Leanne Graham"
-        user = jsonplaceholder.get("/users/1", authentication=False)
+        data = {
+            "id": 4,
+            "name": "Doe",
+            "firstname": "Luc",
+            "title": "Testing Engineer"
+        }
+        user = jsonplaceholder.post("/users", data, authentication=False)
 
-        self.assertEqual(name, user['name'])
+        self.assertEqual(data, user)
+
+    def test_put(self):
+        """
+        Test that we can send a PUT request
+        """
+        # Disable certificate warnings and connect to Rubrik Cluster
+        urllib3.disable_warnings()
+        jsonplaceholder = pythapi.Connect('my-json-server.typicode.com', '/geluyan/pythapi')
+
+        data = {
+            "id": 1,
+            "name": "Doe",
+            "firstname": "Luc",
+            "title": "Testing Engineer"
+        }
+        user = jsonplaceholder.put("/users/1", data, authentication=False)
+
+        self.assertEqual(data, user)
+    
+    def test_delete(self):
+        """
+        Test that we can send a DELETE request
+        """
+        # Disable certificate warnings and connect to Rubrik Cluster
+        urllib3.disable_warnings()
+        jsonplaceholder = pythapi.Connect('my-json-server.typicode.com', '/geluyan/pythapi')
+
+        user = jsonplaceholder.delete("/users/1", authentication=False)
 
 if __name__ == '__main__':
     unittest.main()
